e660abec137fa963add1598be1198ec73fc087de
endikafercu <endika.fernandez-cuesta@bonitasoft.com>
2025-03-13 15:20:54 +0100

    Resolve issues

--- src/test/java/com/bonitasoft/presales/connector/connectorRabbitMQTest.java
+++ src/test/java/com/bonitasoft/presales/connector/connectorRabbitMQTest.java
@@ -4,17 +4,25 @@ import org.bonitasoft.engine.connector.ConnectorException;
 import org.bonitasoft.engine.connector.ConnectorValidationException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
 public class connectorRabbitMQTest {
 
+	@Container
+    private static final RabbitMQContainer rabbitMQContainer = new RabbitMQContainer("rabbitmq:3-management")
+    .withAdminUser("testuser") 
+    .withAdminPassword("testpassword"); 
+	
     connectorRabbitMQ connector;
 
     @BeforeEach
@@ -77,27 +85,34 @@ public class connectorRabbitMQTest {
 
         assertThrows(ConnectorValidationException.class, () -> connector.validateInputParameters());
     }
-    /*
+
+/*
     @Test
     public void testExecuteBusinessLogic() {
         Map<String, Object> parameters = new HashMap<>();
-        parameters.put(RabbitMQConstants.HOST_INPUT_PARAMETER, "localhost");
+        parameters.put(RabbitMQConstants.HOST_INPUT_PARAMETER, rabbitMQContainer.getHost());
+        System.out.println(" ------------------------- rabbitMQContainer.getHost():" + rabbitMQContainer.getHost());
         parameters.put(RabbitMQConstants.QUEUENAME_INPUT_PARAMETER, "BonitaQueue");
         parameters.put(RabbitMQConstants.MESSAGE_INPUT_PARAMETER, "Test Bonita Message");
-        parameters.put(RabbitMQConstants.USERNAME_INPUT_PARAMETER, "admin"); 
-        parameters.put(RabbitMQConstants.PASSWORD_INPUT_PARAMETER, "password"); 
+        System.out.println(" ------------------------- rabbitMQContainer.getAdminUsername():" + rabbitMQContainer.getAdminUsername());
+        System.out.println(" ------------------------- rabbitMQContainer.getAdminPassword():" + rabbitMQContainer.getAdminPassword());
+        parameters.put(RabbitMQConstants.USERNAME_INPUT_PARAMETER, "testuser"); 
+        parameters.put(RabbitMQConstants.PASSWORD_INPUT_PARAMETER, "testpassword"); 
 
         connector.setInputParameters(parameters);
+
         try {
-           
+            
             Map<String, Object> outputs = connector.execute();
             assertThat(outputs)
             .containsKey(RabbitMQConstants.RECEIVEDMESSAGE_OUTPUT_PARAMETER)
             .satisfies(map -> assertThat(map.get(RabbitMQConstants.RECEIVEDMESSAGE_OUTPUT_PARAMETER)).isNotNull());
         } catch (ConnectorException e) {
+             e.printStackTrace();
             fail("Execution failed: " + e.getMessage());
         }
     }
     */
+
     
 }
